7fc6c726986527645637105beef060041269b822
sxz155 <sahar.zarmehri@gmail.com>
2016-11-14 20:08:13 -0500

    this is neccessary

--- tes1.R
+++ tes1.R
@@ -1,3 +1,15 @@
 #add
 
 
+df_test <- data.frame(x = rnorm(10), y = rbinom(10, 1, .5))
+
+adder <- function(df) {
+  
+  sum <- df[,1] + df[,2]
+  product <- df[,1] * df[,2]
+  
+  out <- sum + product
+  out
+}
+
+y <- adder(df_test)
